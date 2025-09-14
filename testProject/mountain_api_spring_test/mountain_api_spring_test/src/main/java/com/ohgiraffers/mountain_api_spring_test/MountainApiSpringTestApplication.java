1609334cb33b9a944389eb5a6662c7f3b0b4868b
kyuhon <kyuhon123@gmail.com>
2025-09-15 04:02:49 +0900

    [create] 마이바티스와 mountain_api 사용방법 학습 중

---
+++ testProject/mountain_api_spring_test/mountain_api_spring_test/src/main/java/com/ohgiraffers/mountain_api_spring_test/MountainApiSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.mountain_api_spring_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountainApiSpringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MountainApiSpringTestApplication.class, args);
+    }
+
+}
---
+++ testProject/mountain_api_spring_test/mountain_api_spring_test/src/test/java/com/ohgiraffers/mountain_api_spring_test/MountainApiSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.mountain_api_spring_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MountainApiSpringTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
