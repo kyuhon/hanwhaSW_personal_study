edee4202ebd571ad6fc72aaf84d13bc511fbca4f
kyuhon <kyuhon123@gmail.com>
2025-09-23 12:38:47 +0900

    [create] 추가학습

---
+++ 11_msa/chap02-01-first-service-lecture-source/src/test/java/com/bughunter/chap0201firstservicel/Chap0201FirstServiceLectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bughunter.chap0201firstservicel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap0201FirstServiceLectureSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
