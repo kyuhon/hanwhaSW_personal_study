5c35cd1932b53c1d5ee9b92e24f7ed3aadb3062d
kyuhon <kyuhon123@gmail.com>
2025-11-01 15:49:37 +0900

    [create] 금요일 학습

---
+++ 16_devops/chap01_01_bootproject/src/test/java/com/ohgiraffers/bootproject/controller/CalculatorControllerTest.java
@@ -0,0 +1,24 @@
+package com.ohgiraffers.bootproject.controller;
+
+import com.ohgiraffers.bootproject.dto.CalculatorDTO;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorControllerTest {
+
+
+
+//    @DisplayName("두 수의 합 구하기 테스트")
+//    @ParameterizedTest
+//    @MethodSource("provideDTOSources")
+//    void testTwoNumbers(CalculatorDTO input){
+//        int expected = input.getNum1() + input.getNum2();
+//        int actual = calculatorService.plusTwoNumbers(input);
+//
+//        // Assertions.assertEquals(expected, actual);
+//        assertEquals(expected, actual);
+//    }
+}
